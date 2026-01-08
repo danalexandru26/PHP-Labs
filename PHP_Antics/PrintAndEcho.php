8f983f79122545653c837f9b468a507f51757324
Alexandru <dan.bulzan@student.upt.ro>
2026-01-08 19:54:03 +0200

    pph echo and print

---
+++ PHP_Antics/PrintAndEcho.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Echo and Print in PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center my-4">Echo and Print in PHP</h1>
+        <hr class="w-50 mx-auto">
+
+        <!-- First centered column -->
+        <div class="row justify-content-center my-2">
+            <div class="col-6">
+                <p>This document will cover the differences between the two most used PHP output functions:
+                    <code>Echo</code> and <code>Print</code>.
+                </p>
+                <p>For all we know, <code>Echo</code> is extremely similar to our shell's display function. It displays
+                    the content between quotation marks and parses HTML tags.</p>
+            </div>
+        </div>
+
+        <!-- Second centered column -->
+        <div class="row justify-content-center my-2">
+            <div class="col-6">
+                <p>In the examples below, both the PHP output and the code snippet that generated it will be provided.
+                </p>
+            </div>
+        </div>
+
+        <!-- Code snippet side-by-side example (if needed) -->
+        <div class="row justify-content-center my-2">
+            <div class="col-6">
+                <pre class="borderS">
+&lt;?php
+ <span style="color:red;">$var</span> = "Bella";
+ <span style="color:red;">echo</span> "My Border Collie is named <span style="color:red;">$var</span>&lt;br&gt;";
+?&gt;
+            </pre>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+
+
+</html>
