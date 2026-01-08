1d6db1dbe68360851ae30f5fa6b0e65e72a242a4
Alexandru <dan.bulzan@student.upt.ro>
2026-01-08 18:36:41 +0200

    initial commit -php-

---
+++ PHP_Antics/IntroductionToPHP.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $color = "maroon";
+    echo "$color is my favorite hue of red";
+
+    //in PHP variables are loosely typed. Is it a dynamically typed language.
+    
+    $myInteger = 5;
+    $x = $y = $z = 10;
+
+    $result = $myInteger + $x;
+    echo "<br>$result";
+    ?>
+
+</body>
+
+</html>
