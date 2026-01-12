046e6291d3c59e24a90305fad36beda84b32092e
Dan <79413476+danalexandru26@users.noreply.github.com>
2026-01-12 23:12:06 +0200

    form validation progress

--- PHP_Forms/PHPFormValidation.php
+++ PHP_Forms/PHPFormValidation.php
@@ -12,22 +12,71 @@
     <link rel="stylesheet" href="../Styles/styling.css">
 </head>
 
-<body style="font-size:18px;">
+<body class="medium">
     <div class="container">
         <main>
-            <div class="row my-2">
-                <div class="col-2">
-                    <aside>
-                        <p>test</p>
-                    </aside>
+            <div class="row pt-4">
+                <aside class="col-2 d-none d-lg-block ps-4">
+                    <div class="position-sticky pt-5" style="top: 4rem">
+                        <ul class="list-unstyled my-4">
+                            <li><a href="http://localhost/PHP_Labs/PHP_Forms/PHPFormManip.php" target="_blank"
+                                    class="text-decoration-none">PHP Forms</a></li>
+                            <li><a href="http://localhost/PHP_Labs/PHP_Forms/PHPGetDispatch.php" target="_blank"
+                                    class="text-decoration-none">PHP Dispatch - GET</a></li>
+                            <li><a href="http://localhost/PHP_Labs/PHP_Forms/PHPPostDispatch.php" target="_blank"
+                                    class="text-decoration-none">PHP Dispatch - POST</a></li>
+                            <li>
+                                <a href="http://localhost/PHP_Labs/PHP_Antics/PrintAndEcho.php" target="_blank"
+                                    class="text-decoration-none">Print and Echo in PHP</a>
+                            </li>
+                            <li> <a href="http://localhost/PHP_Labs/PHP_Antics/DataTypes.php" target="_blank"
+                                    class="text-decoration-none">PHP Data Types</a></li>
+                            <li> <a href="http://localhost/PHP_Labs/PHP_Antics/CastingAndUtilityFunctions.php"
+                                    target="_blank" class="text-decoration-none">Casting and numeric utilities</a></li>
+                            <li><a href="http://localhost/PHP_Labs/PHP_Antics/Constants.php" target="_blank"
+                                    class="text-decoration-none">Constants</a></li>
+                            <li><a href="http://localhost/PHP_Labs/PHP_Antics/FlowControlAndLoops.php" target="_blank"
+                                    class="text-decoration-none">Control Flow And Loops</a></li>
+                            <li><a href="http://localhost/PHP_Labs/PHP_Antics/Functions.php" target="_blank"
+                                    class="text-decoration-none">Functions in PHP</a></li>
+                            <li><a href="http://localhost/PHP_Labs/PHP_Antics/ArrayManipulation.php" target="_blank"
+                                    class="text-decoration-none">Array Manipulation</a></li>
+                        </ul>
+                    </div>
+                </aside>
+                <div class="col-8">
+                    <h1 class="display-4 text-center mt-0">PHP Form Validation</h1>
+                    <hr>
+                    <div>
+                        <h3 id="formValidation">Form Validation</h3>
+                        <p>The process of form validation involves verifying the authenticity of the data submitted by a
+                            user. This is particularly important when dealing with malitious users that are targeting
+                            our system through malicious data entries. On the other hand, data validation at a form
+                            level is an additional safeguard for the data we insert into our databases.</p>
+
+                        <p>There are multiple ways of validating the user data and safekeeping our web-page and back-end
+                            systems against malicious intrusions. We'll split these verifications into two categories,
+                            namely:</p>
+                        <ul>
+                            <li>Action and URL validation</li>
+                            <li>Input sanitization and filtering</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 id="actionAndUrlValidation">Action and URL Validation</h3>
+                        <p></p>
+                    </div>
                 </div>
-                <div class="col-8 justify-content-center">
-                    <p>test</p>
-                </div>
-                <div class="col-2">
-                    <p>test</p>
-                </div>
-            </div>
+                <aside class="col-2 d-none d-lg-block ps-4">
+                    <div class="position-sticky" style="top: 4rem">
+                        <strong class="d-block my-2" style="color:grey">On this page</strong>
+                        <hr>
+                        <ul class="list-unstyled">
+                            <li><a href="#formValidation">Form Validation</a></li>
+                            <li><a href="#actionAndUrlValidation">Action and URL Validation</a></li>
+                        </ul>
+                    </div>
+                </aside>
         </main>
     </div>
 </body>
