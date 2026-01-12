5b1ff038bf38ada5b86b0fe40909e37d08525d64
Alexandru <dan.bulzan@student.upt.ro>
2026-01-12 17:28:43 +0200

    work on form manipulation

--- PHP_Forms/PHPFormManip.php
+++ PHP_Forms/PHPFormManip.php
@@ -26,7 +26,7 @@
                 </div>
                 <div class="row justify-content-center my-4">
                     <div class="col-8">
-                        <h3>Introduction to PHP Forms</h3>
+                        <h3 id="PHPFormIntrod">Introduction to PHP Forms</h3>
                         <p>The term <em>PHP Forms</em> can be a bit misleading, especially since it does not
                             wholly refer to a different entity when compared with <em>HTML Forms</em>. A PHP Form is not
                             a completely different form entity, but a method of capturing and dispatching the user input
@@ -37,7 +37,7 @@
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-8">
-                        <h3>Superglobal Variables</h3>
+                        <h3 id="SuperglobalVariables">Superglobal Variables</h3>
                         <p>The data we want to send to a form handler is collected, at run-time, in either one of the
                             two
                             following <em>superglobal</em> variables:</p>
@@ -54,7 +54,7 @@
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-8">
-                        <h3>GET vs. POST</h3>
+                        <h3 id="GETvsPOST">GET vs. POST</h3>
                         <p>While <code>get</code> and <code>post</code> can, for the most part, be used to accomplish
                             similar functions, there are some fundamental differences that we need address. Being
                             mindful of these differences will help improve our web-page workflow and make them more
@@ -70,12 +70,35 @@
 
                 <div class="row justify-content-center">
                     <div class="col-8">
-                        <h3>Using GET</h3>
+                        <h3 id="usingGET">Using GET</h3>
+                        <p>Information sent from a form with the GET method is visible to everyone. GET also has limits
+                            on the amount of information to
+                            send. The limitation is about 2000 characters. However, because the variables are displayed
+                            in the URL, it is possible to bookmark the page. This can be useful in some cases.</p>
+
+                        <p>GET may be used for sending non-sensitive data.</p>
+
+                        <p>Note: GET should NEVER be used for sending passwords or other sensitive information!</p>
+
+                        <p>We could technically encrypt the data attached to an URL, but this could prove dangerous as
+                            everything we pass is subject to direct decryption and reverse engineering attempts.
+                            Furthermore, it can also be subject to data tampering and replay attacks, which exposes
+                            sensitive data to malitious actors.</p>
                     </div>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-8">
-                        <h3>Using POST</h3>
+                        <h3 id="usingPOST">Using POST</h3>
+                        <p>Information sent from a form with the POST method is invisible to others (all names/values
+                            are embedded within the body of the HTTP request) and has no limits on the amount of
+                            information to send. There is no limit because data is sent in packets across a network, not
+                            embedded in a fixed-format URL.</p>
+
+                        <p>POST also supports advanced functionality such as support for multi-part binary input while
+                            uploading files to server.</p>
+
+                        <p>However, because the variables are not displayed in the URL, it is not possible to bookmark
+                            the page.</p>
                     </div>
                 </div>
 
